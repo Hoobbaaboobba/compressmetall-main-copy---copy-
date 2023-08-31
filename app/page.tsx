3a6887b1b0f154158a0c16b97b421480774dbef7
Hoobbaboobba <xopoxopox@gmail.com>
2023-08-31 22:21:41 +0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import { Metadata } from "next";
 import MainSection from "./components/Home/MainSection/MainSection";
 import Advantages from "./components/Home/advantages/Advantages";
+import { Suspense } from "react";
+import Image from "next/image";
+import LoadingAGif from "./assets/loading.gif";
 
 export const metadata: Metadata = {
   title: "Главная",
+};
+
+const Loading = () => {
+  return <Image src={LoadingAGif} alt="loading" width={100} height={100} />;
 };
 
 export default function Home() {
   return (
     <main className="flex justify-center">
       <div className="max-w-[1050px] w-full">
-        <MainSection />
-        <Advantages />
+        <Suspense fallback={<Loading />}>
+          <MainSection />
+          <Advantages />
+        </Suspense>
       </div>
     </main>
   );
