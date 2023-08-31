c36d7447d2f066c6bc04e333c7b52ed1675047e8
Hoobbaboobba <xopoxopox@gmail.com>
2023-08-31 19:55:45 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,19 @@ export default function RootLayout({
           gtag('config', 'G-5DESE4JXJL');
         `}
       </Script>
+      <Script type="text/javascript">
+        {`   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+       
+          ym(94803385, "init", {
+               clickmap:true,
+               trackLinks:true,
+               accurateTrackBounce:true
+          });`}
+      </Script>
       <body>
         <Header />
         <div className="flex w-full justify-center mt-[80px] xl:mt-[130px]">
